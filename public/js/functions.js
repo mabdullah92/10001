409c5d628d2d109d5331f043bdb433d8e62e6388
user <user@dell>
2015-02-17 19:43:17 +0500

    12.0 first

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,45 +1,52 @@
 function addUser() {
-	    $.ajax({
-    	type: "POST",
-        url: "http://192.168.83.130/demoapp/public/app/prod",
-        cache:false,
-        data: $("#addForm").serialize()}).
-        done(function (msg){
-        	
-        	window.location="http://192.168.83.130/demoapp/public/app/app#nav/grid";
-        	
-        });  
+	$
+			.ajax({
+				type : "POST",
+				url : "http://192.168.83.130/demoapp/public/app/prod",
+				cache : false,
+				data : $("#addForm").serialize()
+			})
+			.done(
+					function(msg) {
+
+						window.location = "http://192.168.83.130/demoapp/public/app/app#nav/grid";
+
+					});
 }
 function editUser(click) {
-	var id=click.id;
-	
-   $.ajax({
-	type: "POST",
-    url: "http://192.168.83.130/demoapp/public/app/edit",
-    cache:false,
-    data: { userId : id }}).
-    done(function (msg){ 
-    	console.log(msg);
-    	 window.location.href="#nav/edit";
-    	
-    }); 
- 
+	var id = click.id;
+
+	$.ajax({
+		type : "POST",
+		url : "http://192.168.83.130/demoapp/public/app/edit",
+		cache : false,
+		data : {
+			userId : id
+		}
+	}).done(function(msg) {
+		console.log(msg);
+		window.location.href = "#nav/edit";
+
+	});
+
 }
 function loginUser() {
-	
-   $.ajax({
-	type: "POST",
-    url: "http://192.168.83.130/demoapp/public/app/login",
-    cache:false,
-    data: $("#loginForm").serialize()}).
-    done(function (msg){
-    	if(msg=="success"){
-    	//	console.log(msg);
-    window.location.href="#nav/grid";
-    	}
-    	else{
-    		//console.log(msg);
-    		 document.getElementById("validuser").innerHTML = "Incorrect Credentials";
-    	}
-    }); 
+
+	$
+			.ajax({
+				type : "POST",
+				url : "http://192.168.83.130/demoapp/public/app/login",
+				cache : false,
+				data : $("#loginForm").serialize()
+			})
+			.done(
+					function(msg) {
+						if (msg == "success") {
+							// console.log(msg);
+							window.location.href = "#nav/grid";
+						} else {
+							// console.log(msg);
+							document.getElementById("validuser").innerHTML = "Incorrect Credentials";
+						}
+					});
 }
