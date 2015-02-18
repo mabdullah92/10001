61e02ba2b9263ebc169acbf3640f8f6cfedfa693
user <user@dell>
2015-02-18 11:15:14 +0500

    asdasd

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,5 @@
 function addUser() {
+
 	$
 			.ajax({
 				type : "POST",
@@ -13,20 +14,56 @@ function addUser() {
 
 					});
 }
-function editUser(click) {
+function setEditUser(click) {
 	var id = click.id;
 
 	$.ajax({
 		type : "POST",
-		url : "http://192.168.83.130/demoapp/public/app/edit",
+		url : "http://192.168.83.130/demoapp/public/app/setedit",
 		cache : false,
 		data : {
 			userId : id
 		}
 	}).done(function(msg) {
-		console.log(msg);
+		var data = JSON.parse(msg);
+		var name = data.name;
+		var pdwd = data.id;
+		$("#editdisp").val(name);
+		$("#stateEdit").val(id);
 		window.location.href = "#nav/edit";
+	});
 
+}
+function editUser() {
+	var id = $("#stateEdit").val();
+	var newName = $("#editdisp").val();
+	$.ajax({
+		type : "POST",
+		url : "http://192.168.83.130/demoapp/public/app/edit",
+		cache : false,
+		data : {
+			editId : id,
+			newName : newName
+		}
+	}).done(function(msg) {
+		window.location.href = "#nav/grid";
+	});
+
+}
+function deleteUser(click) {
+	var id = click.id;
+	$.ajax({
+		type : "POST",
+		url : "http://192.168.83.130/demoapp/public/app/delete",
+		cache : false,
+		data : {
+			delId : id
+		}
+	}).done(function(msg) {
+		// var data=JSON.parse(msg);
+		// var name = data.name;
+		// $("#editdisp").val(name);
+		window.location.href = "#nav/grid";
 	});
 
 }
